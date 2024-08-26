cb2d6abfbad29ee5188307292c5ce5c07a7b44a8
Mike-KZN <mikebd@protonmail.com>
2024-08-26 18:27:54 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,41 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie App :cup_with_straw:")
+
+st.write("Choose the fruits you want in your custom Smoothie:")
+
+# Input for the name on the smoothie order
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')).to_pandas()
+
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe['FRUIT_NAME'].tolist(),
+    max_selections = 5
+)
+
+if name_on_order and ingredients_list:
+    ingredients_string = ' '.join(ingredients_list)
+    
+    my_insert_stmt = f"""
+    insert into smoothies.public.orders(name_on_order, ingredients) 
+    values ('{name_on_order}', '{ingredients_string}')
+    """
+
+    time_to_insert = st.button('Submit Order', key='submit_order')
+    
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+else:
+    if not name_on_order:
+        st.warning('Please enter a name for your smoothie.')
+    if not ingredients_list:
+        st.warning('Please select at least one ingredient.')
