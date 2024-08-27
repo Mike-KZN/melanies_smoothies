e41832153e02997e4a488abc71ae1a94517d176b
Mike-KZN <mikebd@protonmail.com>
2024-08-27 12:11:00 +0200

    Update streamlit_app.py
    dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,5 @@ else:
 # New section to display FruityVice Nutrician Information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
